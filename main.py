dbaa719bd82aae0dac66adddd57968da43c1c446
JudeSurin <74893645+JudeSurin@users.noreply.github.com>
2024-04-19 10:51:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -333,6 +333,9 @@ else:
             display_mileage_info_future()
 
 
+            chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+            st.bar_chart(chart_data)
 
 
 
